faaa139e4efb84a7d61a480e40afc00ee3e7d7e7
Mart <mart@paperplane.ai>
2024-01-19 16:17:17 +0000

    updated default hyperparams

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def get_args():
     parser.add_argument("--per_device_train_batch_size", type=int, default=4)
     parser.add_argument("--gradient_accumulation_steps", type=int, default=10)
     parser.add_argument("--learning_rate", type=float, default=2e-4)
-    parser.add_argument("--num_train_epochs", type=int, default=1)
+    parser.add_argument("--num_train_epochs", type=int, default=2)
     parser.add_argument("--weight_decay", type=float, default=0.01)
     parser.add_argument("--lora_r", type=int, default=128)
     parser.add_argument("--lora_alpha", type=float, default=256)
